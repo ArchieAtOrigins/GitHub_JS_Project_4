09d3cb8c1d2d9d0f8dde4777080015c0b5e71baf
ArchieAtOrigins <archie@originscreative.com>
2017-02-18 13:00:39 -0800

    Working on getting rid of alerts and connecting to id in html. Not yet working although code seems to match ninja1 repos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-quiz = {
+var quiz = {
 	"name": "Super Hero Name Quiz",
 	"description": "How many super heroes can you name?",
 	"question": "What is the real name of ",
@@ -9,37 +9,60 @@ quiz = {
 		]
 }
 
-var score = 0;
+// dom references
+var $question = document.getElementById('question');
+var $score = document.getElementById('score');
+var $feedback = document.getElementById('feedback');
+
+// view functions
+
+function update(element, content, klass) {
+	var p = element.firstChild || document.createElement('p');
+	p.textContent = content;
+	element.appendChild(p);
+	if(klass) {
+		p.className = klass;
+	}
+}
 
 play(quiz);
 
+// function definitions
+
 function play(quiz) {
+	var score = 0;
+	update($score,score);
+
 	// main game loop
 	for(var i=0, question, answer, max=quiz.questions.length; i<max; i++) {
 		question = quiz.questions[i].question;
 		answer = ask(question);
 		check(answer);
 	}
-	//end of game main loop
+	// end of game main loop
 	gameOver();
 
+	// nested functions
+
 	function ask(question) {
-		return prompt(quiz.question + question); //quiz [i] [0] is th ith question
+		update($question,quiz.question + question);
+		return prompt("Enter your answer:");
 	}
 
 	function check(answer) {
 		if(answer === quiz.questions[i].answer) {
-			alert("Correct!");
+			update($feedback,"Correct!","right");
 			// increase score by 1
 			score++;
+			update($score, score);
 		} else {
-			alert("Sorry, that is incorrect");
+			update($feedback,"Sorry, that is incorrect", "wrong");
 		}
 	}
 
 	function gameOver(){
 		// inform the player that the game has finished and tell them their score
-		alert("Game Over. Your score is " + score + " points");
+		update($question, "Game Over, you scored " + score + " points" );
 	}
 }
 
